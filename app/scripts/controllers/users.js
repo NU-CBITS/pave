d782329ff310588cb56e30d2160d7600dd67b78a
Mark Begale <m.begale@gmail.com>
2015-03-19 23:47:26 -0500

    updated router

--- app/scripts/controllers/users.js
+++ app/scripts/controllers/users.js
@@ -14,19 +14,27 @@ angular.module('paveApp')
     $scope.subNavExists = true;
 
     $scope.subNavItems = [
-	    {html:'Administrators',userType:'administrators',href:'#/users/administrators',class:''},
-	    {html:'Providers',userType:'providers',href:'#/users/providers',class:''},
+	    {html:'Staff',userType:'staff',href:'#/users/staff',class:''},
 	    {html:'Clients',userType:'clients',href:'#/users/clients',class:''},
     ];
 
-    $scope.userType     = $routeParams.userType || 'administrators';
+    $scope.userType     = $routeParams.userType || 'staff';
     $scope.pageTitle    = $filter('filter')($scope.subNavItems,{userType:$scope.userType},true)[0].html;
     
     $scope.users = function (){ 
         var contents = {}
+
+        contents.clients = $filter('filter')(Users.users, {isClient:true},true);
+
+        //four major study staff groups
         contents.administrators = $filter('filter')(Users.users, {isAdmin:true},true);
         contents.providers = $filter('filter')(Users.users, {isProvider:true},true);
-        contents.clients = $filter('filter')(Users.users, {isClient:true},true);
+        contents.PDRcallers = $filter('filter')(Users.users, {isPDRcaller:true},true);
+        contents.supervisors = $filter('filter')(Users.users, {isSupervisor:true},true);
+
+        //aggregation of all staff
+        contents.staff = $filter('filter')(Users.users, {isClient:false},true);
+
         return contents[$scope.userType];
     }
 
