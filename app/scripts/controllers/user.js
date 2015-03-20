c33d859cb694d7cb339d4d6e16088a7b704256b9
Mark Begale <m.begale@gmail.com>
2015-03-20 00:17:30 -0500

    add assigned clients/supervisees
    also updated validation process

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -13,8 +13,11 @@ angular.module('paveApp')
 
 
   	$scope.userId       = $routeParams.userId || null;
-  	$scope.user         = Users.get($scope.userId) || { id:uuid4.generate(), isAdmin:false, isProvider:false, isClient:false, isPDRcaller:false, isSupervisor:false, createdAt:new Date() };
+  	$scope.user         = Users.get($scope.userId) || { id:uuid4.generate(), isAdmin:false, isProvider:false, isClient:false, isPDRcaller:false, isSupervisor:false, supervisees:[], assignedClients:[], createdAt:new Date() };
     $scope.alertMessage = '';
+
+    $scope.clients = $filter('filter')(Users.users, {isClient:true},true);
+    $scope.providers = $filter('filter')(Users.users, {isProvider:true},true);
 
   	$scope.saveUser = function(nextLocation){
 
@@ -22,11 +25,7 @@ angular.module('paveApp')
       
       if ($scope.user.isAdmin == false && $scope.user.isProvider == false && $scope.user.isClient == false && $scope.user.isSupervisor == false && $scope.user.isPDRcaller == false) {
         $scope.alertMessage = 'You must select a role!'
-
-      } else if ( ( $scope.user.isAdmin == true || $scope.user.isProvider == true || $scope.user.isPDRcaller == true || $scope.user.isSupervisor == true) && $scope.user.isClient == true ) {
-        $scope.alertMessage = 'Admins, providers, supervisors and PDR callers cannot be clients!'
-
-      } else {
+      }else {
         Users.upsert($scope.user);
         $location.url(nextPage);
       }      
